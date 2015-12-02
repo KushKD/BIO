6cade3a5d952e091d45140fbe0fee583d40b7565
kuush <kushkumardhawan@gmail.com>
2015-12-02 16:06:09 +0530

    Working ... Everything .. This is the initial Commit

---
+++ app/src/main/java/com/nitgen/SDK/AndroidBSP/FingurePoJo.java
@@ -0,0 +1,19 @@
+package com.nitgen.SDK.AndroidBSP;
+
+/**
+ * Created by kuush on 12/2/2015.
+ */
+public class FingurePoJo {
+
+
+
+   public String Fingure_DB ;
+
+    public String getFingure_DB() {
+        return Fingure_DB;
+    }
+
+    public void setFingure_DB(String fingure_DB) {
+        Fingure_DB = fingure_DB;
+    }
+}
